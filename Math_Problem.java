5870de3ceae9b9a7dcf0ef0c99856f873e00a710
andrewjhage <ahage123@gmail.com>
2022-11-05 13:09:55 -0400

    Add files via upload

---
+++ Math_Problem.java
@@ -0,0 +1,12 @@
+/*Andrew Hage
+ * 10/25/2022
+ * Module 1.5 Math Equation
+ * SDEV200
+ */
+
+public class Math_Problem {
+    public static void main(String[] args) {
+        // Display information
+        System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+    }
+}
